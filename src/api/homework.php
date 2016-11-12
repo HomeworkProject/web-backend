ffdb0421cb41f89f0f03d81c8e2f2851994b7309
Sebastian Paarmann <sebastian.paarmann@gmail.com>
2016-11-12 15:49:50 +0100

    Initial commit

---
+++ src/api/homework.php
@@ -0,0 +1,13 @@
+<?php
+
+echo '
+  [
+    {
+      id: "20123",
+      title: "Kondensatoren Graph",
+      date: "2016-11-14",
+      subject: "Physik",
+      desc: "Graph zu Messergebnissen zeichnen.",
+    },
+  ]
+';
